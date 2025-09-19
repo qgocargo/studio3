5750803f54ed13e0892f948d17a6642debf22416
akif bade <kiffbade@gmail.com>
2025-09-19 06:31:35 +0000

    Try fixing this error: `Console Error: Invalid prop `data-sidebar` suppl

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -66,7 +66,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
           <SidebarMenu>
             {menuItems.map((item) => (
               <SidebarMenuItem key={item.href}>
-                <Link href={item.href}>
+                <Link href={item.href} passHref>
                   <SidebarMenuButton
                     isActive={pathname.startsWith(item.href)}
                     tooltip={item.label}
